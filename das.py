102f01aa8125e558771d1996cf8e74f392483651
Albert Fajula <fajulanet@gmail.com>
2015-10-19 13:36:39 +0200

    added reading exception and automatic-data reading

--- das.py
+++ das.py
@@ -15,24 +15,50 @@ from datetime import datetime
 class esdeveniment:
     
     def __init__(self, event):
-        self.code=event[0]
-        self.date=datetime.strptime(event[1],'%d/%m/%Y')
-        self.time=datetime.strptime(event[2],'%H:%M:%S.%f')
-        self.date=self.date.replace(hour=self.time.hour, 
-                                    minute=self.time.minute, second=self.time.second, 
-                                    microsecond=self.time.microsecond)
-        self.lat=float(event[3])
-        self.lon=float(event[4])
-        '''print self.lon
-        if self.lon[-1]=='W':
-            self.lon=float(event[4][:-2])*-1
-        else:
-            self.lon=float(event[4][:-2])'''
-        self.prof=int(event[5])
-        self.mag=float(event[6])
+        try:
+            self.code=event[0]
+            self.date=datetime.strptime(event[1],'%d/%m/%Y')
+            self.time=datetime.strptime(event[2],'%H:%M:%S.%f')
+            self.date=self.date.replace(hour=self.time.hour, 
+                                        minute=self.time.minute, second=self.time.second, 
+                                        microsecond=self.time.microsecond)
+            self.lat=float(event[3])
+            self.lon=float(event[4])
+            '''print self.lon
+            if self.lon[-1]=='W':
+                self.lon=float(event[4][:-2])*-1
+            else:
+                self.lon=float(event[4][:-2])'''
+            self.prof=int(event[5])
+            self.mag=float(event[6])
+        except ValueError:
+            pass
 
 
-
+class esdeveniment_automatic(esdeveniment):
+    
+    def __init__(self,event):
+        try:
+            esdeveniment.__init__(self,event)
+            self.rms=float(event[7])
+            self.mindist=float(event[8])
+            self.gap=float(event[9])
+            self.numfases_loc=float(event[10])
+            self.nummags=float(event[11])
+            self.desvest=float(event[12])
+            self.codibd=event[13]
+            self.numfases_lec=float(event[14])
+            self.eix_max=float(event[15])
+            self.azm_max=float(event[16])
+            self.inc_max=float(event[16])
+            self.eix_mig=float(event[18])
+            self.azm_mig=float(event[19])
+            self.inc_mig=float(event[20])
+            self.eix_min=float(event[21])
+            self.azm_min=float(event[22])
+            self.inc_min=float(event[23])
+        except ValueError:
+            pass
 #t = Table.read('manual.csv', format='csv'''', dtype=('S8','S10','S11','f5','f5','i2','f4')''')
 '''
 t = ascii.read('manual.csv', delimiter=',')
@@ -41,9 +67,13 @@ for i in range(len(t)):
     esdeveniments[i]=esdeveniment_manual(t[i])'''
 
 #a=esdeveniment(['l1510962','13/10/2015','20:11:42.10','42.55 N','1.96 E','8','2.0'])
-eventlist=[]
+manuallist=[]
 with open('manual.csv') as csvfile:
     eventreader = csv.reader(csvfile)
-    eventlist=[esdeveniment(row) for row in eventreader]
-    
+    manuallist=[esdeveniment(row) for row in eventreader]
+
+autolist=[]
+with open('automatic.csv') as csvfile:
+    eventreader = csv.reader(csvfile)
+    autolist=[esdeveniment_automatic(row) for row in eventreader]
 
